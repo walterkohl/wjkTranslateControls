f786699bef1be6a28e95f8ff26b5ef8b8c752913
Walter Kohl <walter.kohl@hotmail.com>
2013-08-24 19:44:11 +0700

    Create tConfirmButton.cs
    It's the equivalent to ASP.net control "Button" and translates "Text" and has implemented a "ConfirmationMessage".

---
+++ wjkTranslateControls/tConfirmButton.cs
@@ -0,0 +1,122 @@
+/* ===================================================
+ * wjkTranslateControls
+ * http://help.thaifarang.net/wjk-software.wjkTranslateControls
+ * ===================================================
+ * (c) Copyright 2013 Dipl.-Ing. Walter Kohl - wjk-Software(R)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Threading;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Collections;
+
+namespace wjkTranslateControls
+{
+    [DefaultProperty("Button")]
+    [ToolboxData("<{0}:tConfirmButton runat=server></{0}:tConfirmButton>")]
+    public class tConfirmButton : System.Web.UI.WebControls.Button
+    {
+        translator trans = new translator();
+
+        [Category("ToTranslate")]
+        [Description("The text of the confirmation dialog. Without, it doesn't work.")]
+        [DefaultValue("")]
+        [Bindable(true)] 
+        [Localizable(true)]
+        public string ConfirmationMessage
+        {
+            get
+            {
+                string temp = (setPage != null) ? setPage : strPage;
+                if (ViewState["ConfirmationMessage"] == null) return string.Empty;
+                return trans.translate(temp, strID, "ConfirmMessage", strLanguage, ViewState["ConfirmationMessage"].ToString());
+            }
+            set 
+            { 
+                ViewState["ConfirmationMessage"] = value; 
+            }
+        }
+
+        [Bindable(true)]
+        [Category("ToTranslate")]
+        [DefaultValue("")]
+        [Localizable(true)]
+        [Description("Set or get the text of the button.")]
+        public new string Text
+        {
+            get
+            {
+                string temp = (setPage != null) ? setPage : strPage;
+                String s = (String)ViewState["Text"];
+                return ((s == null) ? String.Empty : trans.translate(temp, strID, "Text", strLanguage, s));
+            }
+            set
+            {
+                ViewState["Text"] = value;
+            }
+        }
+
+        protected override void AddAttributesToRender(HtmlTextWriter writer)
+        {
+
+            if (!string.IsNullOrEmpty(ConfirmationMessage))
+            {
+                string _ConfirmationMessage = string.Format("if(!confirm('{0}')) return false;", ConfirmationMessage.Replace("'", "\'"));
+
+                writer.AddAttribute(System.Web.UI.HtmlTextWriterAttribute.Onclick, _ConfirmationMessage);
+            }
+            base.AddAttributesToRender(writer);
+        }
+
+        public string strLanguage
+        {
+            get
+            {
+                if (this.Page == null) { return string.Empty; } else { return Thread.CurrentThread.CurrentUICulture.Name.Substring(0, 2); }
+            }
+        }
+
+        public string strID
+        {
+            get { return this.ClientID.ToString(); }
+        }
+
+        public string strPage
+        {
+            get
+            {
+                if (!DesignMode)
+                    return this.Page.Request.AppRelativeCurrentExecutionFilePath;
+                else return string.Empty;
+            }
+        }
+        [Bindable(true)]
+        [Category("ToTranslate")]
+        [DefaultValue("")]
+        [Localizable(true)]
+        [Description("If a page must be set.")]
+        public string setPage { set; get; }
+    }
+}
